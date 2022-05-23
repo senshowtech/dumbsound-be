e5b2683e5dae40b9d53141a1fcc4dfb69341fab3
sena <hargiantofebrisena@gmail.com>
2022-05-23 23:22:38 +0700

    initial

--- src/cron/cronServer.js
+++ src/cron/cronServer.js
@@ -1,3 +1,4 @@
+const { date } = require("joi");
 const cron = require("node-cron");
 const { transaction, music, user } = require("../../models");
 
@@ -15,8 +16,45 @@ const getAllTransactionAdmin = async (req, res) => {
     dataTransaction.forEach((element) => {
       let dateDb = new Date(element.endDate);
       let dateNow = new Date();
+      if (
+        dateDb.getDate() === dateNow.getDate() &&
+        dateDb.getMonth() === dateNow.getMonth() &&
+        dateDb.getFullYear() === dateNow.getFullYear()
+      ) {
+        updateTransaction("failed", element.id);
+        console.log(element.status);
+      }
+      if (element.status === "failed") {
+        updateEndDate("null", element.id);
+      }
     });
   } catch (error) {
     console.log(error);
   }
+};
+
+const updateTransaction = async (status, transactionId) => {
+  await transaction.update(
+    {
+      status,
+    },
+    {
+      where: {
+        id: transactionId,
+      },
+    }
+  );
+};
+
+const updateEndDate = async (endDate, transactionId) => {
+  await transaction.update(
+    {
+      endDate,
+    },
+    {
+      where: {
+        id: transactionId,
+      },
+    }
+  );
 };
